2268afd5c25d00cdbfe9e667605940851f9cf168
Harsh <harshvirat894@gmail.com>
2025-03-13 16:47:28 +0530

    Spring

--- myFirstApp/src/main/java/com/harsh/springboot/myFirstApp/rest/FunRestController.java
+++ myFirstApp/src/main/java/com/harsh/springboot/myFirstApp/rest/FunRestController.java
@@ -1,10 +1,17 @@
 package com.harsh.springboot.myFirstApp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    @Value("${dev.name}")
+    private String devName;
+
+    @Value("${dev.age}")
+    private String devAge;
 
     @GetMapping("/")
     public String sayHello() {
@@ -13,6 +20,6 @@ public class FunRestController {
 
     @GetMapping("/harsh")
     public String sayHarsh() {
-        return "Hello Harsh! from Java ";
+        return "Hello " + devName + "! from Java. " + "Your age is " + devAge;
     }
 }
