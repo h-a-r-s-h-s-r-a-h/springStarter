bdaa18d670e72e4836ac885a8f4e5473246318ef
Harsh <harshvirat894@gmail.com>
2025-03-14 17:58:10 +0530

    Spring

---
+++ Project02/myFirstApp/src/main/java/com/harsh/springboot/myFirstApp/MyFirstAppApplication.java
@@ -0,0 +1,12 @@
+package com.harsh.springboot.myFirstApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFirstAppApplication.class, args);
+    }
+}
