f664ca16e5154becb37683e148860687fe7b460b
Harsh <harshvirat894@gmail.com>
2025-03-15 18:04:50 +0530

    Spring

---
+++ project03/src/main/java/com/dev/springBoot/server/project03/rest/ServerRestController.java
@@ -0,0 +1,30 @@
+package com.dev.springBoot.server.project03.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServerRestController {
+
+    @Value("${dev.name}")
+    private String devName;
+
+    @Value("${dev.age}")
+    private String devAge;
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "hello world!";
+    }
+
+    @GetMapping("/harsh")
+    public String sayHarsh() {
+        return "Hello " + devName + "! from Java. " + "Your age is " + devAge;
+    }
+
+    @GetMapping("/dev")
+    public String sayDev() {
+        return "Hello Dev! from Java. ";
+    }
+}
