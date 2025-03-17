e9867a22d0fe3e4c9f9270064b841e9025f47163
Harsh <harshvirat894@gmail.com>
2025-03-17 18:08:27 +0530

    Spring

--- project06/server/src/main/java/com/dev/server/ServerApplication.java
+++ project06/server/src/main/java/com/dev/server/ServerApplication.java
@@ -3,11 +3,12 @@ package com.dev.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = {"com.dev.server", "com.dev.util"})
 public class ServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
 
 }
---
+++ project06/server/src/main/java/com/dev/util/Coach.java
@@ -0,0 +1,5 @@
+package com.dev.util;
+
+public interface Coach {
+    String getDailyWorkout();
+}
