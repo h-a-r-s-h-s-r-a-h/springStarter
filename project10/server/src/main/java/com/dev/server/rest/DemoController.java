08df0226fb32d1fbc6646af3f2b365050a7048b8
Harsh <harshvirat894@gmail.com>
2025-03-19 16:02:38 +0530

    Spring

--- project10/server/src/main/java/com/dev/server/rest/DemoController.java
+++ project10/server/src/main/java/com/dev/server/rest/DemoController.java
@@ -11,7 +11,7 @@ public class DemoController {
     private Coach myCoach;
 
     @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+    public DemoController(@Qualifier("trackCoach") Coach theCoach) {
         System.out.println("In constructor: " + getClass().getSimpleName());
         myCoach = theCoach;
     }
