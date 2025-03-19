793dd19a7a074454bcc42903c9c4bfa3d81ed863
Harsh <harshvirat894@gmail.com>
2025-03-19 17:31:02 +0530

    Spring

---
+++ project12/server/src/main/java/com/dev/server/rest/DemoController.java
@@ -0,0 +1,23 @@
+package com.dev.server.rest;
+
+import com.dev.server.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    private Coach myCoach;
+
+    @Autowired
+    public DemoController(@Qualifier("trackCoach") Coach theCoach) {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
