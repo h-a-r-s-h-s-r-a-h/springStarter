c5e737e051ac188eb9db26a61fa1e4b38320d431
Harsh <harshvirat894@gmail.com>
2025-03-20 17:08:41 +0530

    Spring

--- project14/server/src/main/java/com/dev/server/common/CricketCoach.java
+++ project14/server/src/main/java/com/dev/server/common/CricketCoach.java
@@ -1,5 +1,7 @@
 package com.dev.server.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -7,8 +9,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class CricketCoach implements Coach {
 
+    //define our init method
+
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("In doMyStartupStuff() : " + getClass().getSimpleName());
+    }
+
+    //define our destroy method
+
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("In doMyCleanupStuff() : " + getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15 minutes";
     }
+
+
 }
