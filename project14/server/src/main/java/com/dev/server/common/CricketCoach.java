33bb1020783c7e46f05cd4feebd42e01bafc7781
Harsh <harshvirat894@gmail.com>
2025-03-20 17:09:57 +0530

    Spring

--- project14/server/src/main/java/com/dev/server/common/CricketCoach.java
+++ project14/server/src/main/java/com/dev/server/common/CricketCoach.java
@@ -2,8 +2,6 @@ package com.dev.server.common;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
