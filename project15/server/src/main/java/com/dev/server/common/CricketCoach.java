7b79470368032eddde8ff63ba0ca004c84925254
Harsh <harshvirat894@gmail.com>
2025-03-20 17:28:41 +0530

    Spring

--- project15/server/src/main/java/com/dev/server/common/CricketCoach.java
+++ project15/server/src/main/java/com/dev/server/common/CricketCoach.java
@@ -7,24 +7,9 @@ import org.springframework.stereotype.Component;
 @Component
 public class CricketCoach implements Coach {
 
-    //define our init method
-
-    @PostConstruct
-    public void doMyStartupStuff() {
-        System.out.println("In doMyStartupStuff() : " + getClass().getSimpleName());
-    }
-
-    //define our destroy method
-
-    @PreDestroy
-    public void doMyCleanupStuff() {
-        System.out.println("In doMyCleanupStuff() : " + getClass().getSimpleName());
-    }
-
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15 minutes";
     }
-
 
 }
--- project15/server/src/main/java/com/dev/server/rest/DemoController.java
+++ project15/server/src/main/java/com/dev/server/rest/DemoController.java
@@ -21,5 +21,4 @@ public class DemoController {
         return myCoach.getDailyWorkout();
     }
 
-
 }
