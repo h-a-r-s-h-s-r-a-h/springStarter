a6e4188e50ffc7aaf4f4ddac0835ced59a4820a8
Harsh <harshvirat894@gmail.com>
2025-03-21 17:06:11 +0530

    Spring

--- project17/server/src/main/java/com/dev/server/dao/StudentDAO.java
+++ project17/server/src/main/java/com/dev/server/dao/StudentDAO.java
@@ -1,4 +1,7 @@
 package com.dev.server.dao;
 
+import com.dev.server.entity.Student;
+
 public interface StudentDAO {
+    void save(Student theStudent);
 }
---
+++ project17/server/src/main/java/com/dev/server/dao/StudentDAOImpl.java
@@ -0,0 +1,13 @@
+package com.dev.server.dao;
+
+import com.dev.server.entity.Student;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO {
+
+    @Override
+    public void save(Student theStudent){
+
+    }
+}
