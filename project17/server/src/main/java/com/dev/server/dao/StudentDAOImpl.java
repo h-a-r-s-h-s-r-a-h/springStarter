9721beeb5657b83905ce16b4b09dcdfceb359473
Harsh <harshvirat894@gmail.com>
2025-03-21 17:08:52 +0530

    Spring

--- project17/server/src/main/java/com/dev/server/dao/StudentDAOImpl.java
+++ project17/server/src/main/java/com/dev/server/dao/StudentDAOImpl.java
@@ -1,13 +1,24 @@
 package com.dev.server.dao;
 
 import com.dev.server.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO {
 
-    @Override
-    public void save(Student theStudent){
+    private EntityManager entityManager;
 
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+        entityManager.persist(theStudent);
     }
 }
