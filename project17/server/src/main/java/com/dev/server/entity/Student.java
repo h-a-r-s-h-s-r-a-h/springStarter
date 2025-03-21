bc760ca87f7b4c86a68f424626fc9ed989f75f74
Harsh <harshvirat894@gmail.com>
2025-03-21 16:41:11 +0530

    Spring

--- project17/server/src/main/java/com/dev/server/entity/Student.java
+++ project17/server/src/main/java/com/dev/server/entity/Student.java
@@ -11,7 +11,12 @@ public class Student {
     @Column(name = "id")
     private int id;
 
-
     @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
 }
