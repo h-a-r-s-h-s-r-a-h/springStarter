130e360808edf1cff319cdf09f8af0ee476fa372
Harsh <harshvirat894@gmail.com>
2025-03-23 02:37:25 +0530

    Spring

---
+++ project22/server/src/main/java/com/dev/server/dao/StudentDAO.java
@@ -0,0 +1,24 @@
+package com.dev.server.dao;
+
+import com.dev.server.entity.Student;
+
+import java.util.List;
+
+public interface StudentDAO {
+
+    void save(Student theStudent);
+
+    Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String theLastName);
+
+    void update(Student theStudent);
+
+    void delete(Integer id);
+
+    int deleteByLastname(String theLastName);
+
+    int deleteAll();
+}
