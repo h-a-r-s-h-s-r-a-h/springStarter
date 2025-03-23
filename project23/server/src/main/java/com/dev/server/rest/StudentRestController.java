b0da35b0969d2e90e8c8e8b87eb3adc0876d6f3e
Harsh <harshvirat894@gmail.com>
2025-03-23 18:13:20 +0530

    Spring

--- project23/server/src/main/java/com/dev/server/rest/StudentRestController.java
+++ project23/server/src/main/java/com/dev/server/rest/StudentRestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -13,7 +14,15 @@ public class StudentRestController {
 
     @GetMapping("/students")
     public List<Student> getStudent() {
-        return null;
+
+        List<Student> theStudents = new ArrayList<>();
+
+        theStudents.add(new Student("Palak", "nunu"));
+        theStudents.add(new Student("Harsh", "dev"));
+        theStudents.add(new Student("Rabindra", "Choudhary"));
+        theStudents.add(new Student("Chhoti", "Kumari"));
+
+        return theStudents;
     }
 
 }
