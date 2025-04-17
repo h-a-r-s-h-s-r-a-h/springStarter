ace35cae4346c3352b005c6956a9e0ef786b35b5
Harsh <harshvirat894@gmail.com>
2025-04-17 19:23:44 +0530

    spring

--- project25/server/src/main/java/com/dev/springBoot/server/rest/EmployeeRestController.java
+++ project25/server/src/main/java/com/dev/springBoot/server/rest/EmployeeRestController.java
@@ -43,4 +43,12 @@ public class EmployeeRestController {
         return dbEmployee;
     }
 
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee) {
+
+        Employee dbEmployee = employeeService.save(theEmployee);
+
+        return dbEmployee;
+    }
+
 }
