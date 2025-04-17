fce42a0155941342c92716c84fc87fbd943360a8
Harsh <harshvirat894@gmail.com>
2025-04-17 15:57:51 +0530

    Spring

--- project23/server/src/main/java/com/dev/server/rest/StudentErrorResponse.java
+++ project23/server/src/main/java/com/dev/server/rest/StudentErrorResponse.java
@@ -13,4 +13,28 @@ public class StudentErrorResponse {
         this.message = message;
         this.timeStamp = timeStamp;
     }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
 }
