5aaf71ce323e115e9c031d322f199abfdb73d16f
Harsh <harshvirat894@gmail.com>
2025-04-17 18:17:23 +0530

    spring

--- project25/server/src/main/java/com/dev/springBoot/server/entity/Employee.java
+++ project25/server/src/main/java/com/dev/springBoot/server/entity/Employee.java
@@ -60,4 +60,14 @@ public class Employee {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
