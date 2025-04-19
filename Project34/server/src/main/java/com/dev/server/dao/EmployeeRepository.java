e9997e13d012ae5bf43cb513fd1a7f55025521f5
Harsh <harshvirat894@gmail.com>
2025-04-19 22:20:13 +0530

    spring

---
+++ Project34/server/src/main/java/com/dev/server/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.dev.server.dao;
+
+
+import com.dev.server.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL!
+
+}
