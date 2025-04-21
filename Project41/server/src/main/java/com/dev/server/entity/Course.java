f0d309da6ed07c9460b9c9aeb5863e4f9fbf12c3
Harsh <harshvirat894@gmail.com>
2025-04-22 04:42:55 +0530

    spring

--- Project41/server/src/main/java/com/dev/server/entity/Course.java
+++ Project41/server/src/main/java/com/dev/server/entity/Course.java
@@ -2,6 +2,9 @@ package com.dev.server.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "course")
 public class Course {
@@ -18,6 +21,9 @@ public class Course {
             CascadeType.DETACH, CascadeType.REFRESH})
     @JoinColumn(name = "instructor_id")
     private Instructor instructor;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Review> reviews;
 
     public Course() {
     }
@@ -49,6 +55,24 @@ public class Course {
     public void setTitle(String title) {
         this.title = title;
     }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    // add a convenience method
+    public void addReview(Review theReview) {
+        if (reviews == null) {
+            reviews = new ArrayList<>();
+        }
+
+        reviews.add(theReview);
+    }
+
 
     @Override
     public String toString() {
