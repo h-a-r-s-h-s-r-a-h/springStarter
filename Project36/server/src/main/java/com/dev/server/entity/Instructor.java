c5e3a787842c9b537d5c3cdd6e598a6ed6547961
Harsh <harshvirat894@gmail.com>
2025-04-21 17:49:06 +0530

    spring

---
+++ Project36/server/src/main/java/com/dev/server/entity/Instructor.java
@@ -0,0 +1,25 @@
+package com.dev.server.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "instructor")
+public class Instructor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private InstructorDetail instructorDetail;
+}
