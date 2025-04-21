0e959123e8ebda75308ea3e0e613c0779d6e2967
Harsh <harshvirat894@gmail.com>
2025-04-21 16:12:44 +0530

    spring

--- Project35/server/src/main/java/com/dev/server/security/DemoSecurityConfig.java
+++ Project35/server/src/main/java/com/dev/server/security/DemoSecurityConfig.java
@@ -18,7 +18,19 @@ public class DemoSecurityConfig {
     // add support for JDBC
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        // define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id=?"
+        );
+
+        // define query to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, roles from roles where user_id=?"
+        );
+
+        return jdbcUserDetailsManager;
     }
 
     @Bean
